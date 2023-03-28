95ef758b31f237f08773db4f643018cd8457ee01
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-28 12:39:33 +0100

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,6 +1,42 @@
+resource "aws_iam_role" "kms_role" {
+  name = "my-kms-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "kms.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "kms_key_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = [aws_iam_role.kms_role.arn]
+      type        = "AWS"
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+    ]
+
+    resources = [
+      aws_kms_key.my_key.arn,
+      aws_kms_key.my_key.arn+":*",
+    ]
+  }
+}
+
 resource "aws_kms_key" "my_key" {
   description = "My KMS key"
-  name = "firstkey"
-  #policy      = jsonencode(data.aws_iam_policy_document.kms_policy.json)
-  policy      =  data.aws_iam_policy_document.kms_policy.json
+  policy      = jsonencode(data.aws_iam_policy_document.kms_key_policy.json)
 }
