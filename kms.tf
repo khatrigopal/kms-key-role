7bbebae5b8cca238179e5f5161aa7793895cd7bf
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-28 12:34:05 +0100

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,4 +1,5 @@
 resource "aws_kms_key" "my_key" {
   description = "My KMS key"
-  policy      = jsonencode(data.aws_iam_policy_document.kms_policy.json)
+  #policy      = jsonencode(data.aws_iam_policy_document.kms_policy.json)
+  policy      =  data.aws_iam_policy_document.kms_policy.json
 }
